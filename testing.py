fb25695094d00bd294397d9a689d50c363200d16
Thorben <mennthor@aol.com>
2015-05-19 15:18:23 +0200

    unfinished response matrix creation

--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ import scipy.stats as scs
 import numpy as np
 import matplotlib.pyplot as plt
 
+###################################
+## plot the test data distribution
 import testdata
 
 # get double normal distributed testdata
@@ -11,6 +13,7 @@ measured, true = testdata.double_gaus()
 x = np.linspace(-0.5, 3.5, 500)
 pdf = .5 * (scs.norm.pdf(x, loc=1., scale=.3) + scs.norm.pdf(x, loc=2., scale=.2))
 
+plt.figure()
 plt.plot(x, pdf, "k-", label="true pdf")
 
 plt.hist(
@@ -23,4 +26,32 @@ plt.hist(
 	)
 
 plt.legend(loc="best")
+
+###################################
+## create the response matrix and plot the binned spline representation of f(x)
+import unfold
+
+# binning of mc_meas in variable y
+bins_meas = np.linspace(0.5, 3.0, 15)
+n_bins_meas = len(bins_meas)
+# binning of the mc_truth f(x) AFTER the unfolding and llh fit. has no effect in this section
+bins_unfold = np.linspace(0.0, 3.0, 20)
+# position of the bspline knots to represent f(x). doesn't have to be equally spaced
+inner_spline_knots = np.arange(0.0, 3.5, 0.5)
+spline_deg = 3
+n_splines = len(inner_spline_knots - spline_deg - 1)
+
+# create the class instance
+blobel_unfold = unfold.Blobel(bins_meas, bins_unfold, inner_spline_knots)
+
+# the function create_response_matrix return the matrix Aij which maps the true variable x to the measured variable y
+A = np.zeros([n_bins_meas, n_splines])
+A = blobel_unfold.build_response_matrix(measured, true)
+
+# plot the columns Aj of A contain the histograms of mc_meas when f(x)=pj(x)
+# plt.figure()
+# for j in range(n_splines):
+# 	plt.plot(bins_meas[:-1], A[:, j])
+
 plt.show()
+
--- unfold.py
+++ unfold.py
@@ -1,44 +1,91 @@
 import numpy as np
+import scipy.interpolate as sci
 
 
 class Blobel():
 	"""
-	docstring for Blobel:
-	- bins_meas: array which sets the binning for all functions dependent on
-				 the measured varible y. The first and last values define
-				 the range of y in which the unfolding operates.
-	- bins_unfold: array which sets the binning of the target function f(x)
-				   which is unfolded. the binning is applied to the spline
-				   representation of f(x). The first and last values define
-				   the range of x in which the target function is defined.
-	- spline_knots: array containing the inner knots for the (cubic) bspline
-					basis functions which are used to represent f(x). the outer
+	Unfolding method after Blobel:
+
+	- bins_meas: Array which sets the binning for all functions dependent on
+				 the measured varible y (Measured MC). The first and last values
+				 define the range of y in which the unfolding operates.
+	- bins_unfold: Array which sets the binning of the target function f(x)
+				   AFTER the unfolding. The binning is applied to the spline
+				   representation of f(x) to get single points with error
+				   estimation. The first and last values define the range
+				   of x in which the target function is defined.
+	- spline_knots: Array containing the inner knots for the (cubic) bspline
+					basis functions which are used to represent f(x). The
+					spline representation is used to discretize the true MC
+					to build the response matrix Aij. The necessary outer
 					knots are automatically added by repeating spline_knots[0]
-					abd spline_knots[-1] 3 times each.
+					and spline_knots[-1] 3 times (cubic splines) each.
 	"""
 	def __init__(self, bins_meas, bins_unfold, spline_knots):
 		self.bins_meas = bins_meas
 		self.bins_unfold = bins_unfold
-		self.range_meas = range_meas
-		self.range_unfold = range_unfold
 		self.spline_knots = spline_knots
 
 		# use cubic splines to represent the unfolded target function f(x)
 		self.spline_deg = 3
 
-		# add the outer knots
-		self.pre = np.zeros(spline_deg)
-		self.post = np.zeros(spline_deg)
-		self.pre.fill(spline_knots[0])
-		self.post.fill(spline_knots[-1])
-		self.spline_knots = np.concatenate((self.pre, self.spline_knots, self.post))
+		# add the outer knots for the splines
+		# self.pre = np.zeros(self.spline_deg)
+		# self.post = np.zeros(self.spline_deg)
+		# self.pre.fill(spline_knots[0])
+		# self.post.fill(spline_knots[-1])
+		# self.spline_knots = np.concatenate((self.pre, self.spline_knots, self.post))
 
-		# number of splines used to represent f(x)
+		# number of splines (=coefficients aj) used to represent f(x)
 		self.n_splines = len(self.spline_knots - self.spline_deg - 1)
 
 
-		def build_response_matrix(self, mc_meas, mc_truth):
-			"""
-			Builds the response matrix Aij where i is in bins_meas and
-			j in spline_knots.
-			"""
+	def build_response_matrix(self, mc_meas, mc_truth):
+		"""
+		Builds the response matrix Aij. i is the number of bins_meas and
+		j is the number of spline_knots. To get Aij we use the mc_truth
+		and mc_meas to track how f(x) is mapped to g(y).
+
+		- mc_truth: True data before the MC detector simulation. f(x) is
+				    the assumed distribution of the true data and is
+				    described by mc_truth.
+		- mc_meas: MC data after the detector simulation resulting from
+				   mc_truth. g(y) is the measured distribution obtained
+				   from MC simulation from f(x) and describes the measured
+				   detector values.
+		"""
+		# response matrix Aij
+		self.rows = len(self.bins_meas) - 1
+		self.cols = self.n_splines
+		self.A = np.zeros([self.rows, self.cols])
+
+		## calculate spline representation of mc_truth f(x).
+		# Every column Aj is a histogram mc_meas in y with binning bins_meas, if f(x) would be described by only one single basis function pj. To get the correct representation, set the histogram weights proportional to the basis function pj. This way, if we add up all hisotgrams Aj, we get the original distribution mc_meas. The correct weights (spline coefficients) aj, which really describe f(x) are found later with the maximum likelihood fit. Here we only create a histogram for every basis function.
+		self.spline_coeff = np.zeros(self.cols)
+		for j in range(self.n_splines):
+			# enable only the jth basis spline
+			self.spline_coeff.fill(0)
+			self.spline_coeff[j] = 1
+			# set the hist weight for Aj(y) proportional to pj(x)
+			self.tck = (self.spline_knots, self.spline_coeff, self.spline_deg)
+			self.basis_hist_weights = sci.splev(mc_meas, self.tck, ext=1)
+			# bin mc_meas with weight according to f(x)=pj(x)
+			self.hist, self.bins = np.histogram(
+				mc_meas,
+				bins=self.bins_meas,
+				range=(self.bins_meas[0], self.bins_meas[1]),
+				normed=False,
+				weights = self.basis_hist_weights
+				)
+			self.A[:, j] = self.hist
+
+		return self.A
+
+
+
+
+
+
+
+
+
