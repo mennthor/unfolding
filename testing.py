incorrect normalization of response matrix has to be fixed

testing.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 import scipy.stats as scs
 import scipy.interpolate as sci
 import numpy as np
@@ -15,18 +16,17 @@ x = pdfvals[0]
 pdf = pdfvals[1]
 
 # Set binning of measured MC (after detector sim) in variable y. Doesn't have to be euqidistant.
-bins_meas = np.linspace(0.0, 3.0, 20)
+bins_meas = np.linspace(0.5, 2.5, 20)
 n_bins_meas = len(bins_meas - 1)
 # Final binning of the unfolded function f(x) AFTER the unfolding and llh fit. Doesn't have to be equal to bins_meas. Not to be confused with the spline knots, which describe the intital discretization of the MC truth f0(x) to build the response matrix.
-bins_unfold = np.linspace(0.0, 3.0, 11)
+bins_unfold = np.linspace(0.0, 3.0, 20)
 n_bins_unfold = len(bins_unfold - 1)
 
 
 ###################################
 ## Create the response matrix
-
 # Position of the bspline knots to represent f0(x). Doesn't have to be equally spaced
-inner_spline_knots = np.arange(0.0, 3.5, 1)
+inner_spline_knots = np.linspace(0.5, 2.5, 15)
 
 # Create the class instance
 blobel_unfold = unfold.Blobel(bins_meas, bins_unfold, inner_spline_knots)
@@ -36,40 +36,49 @@ A = blobel_unfold.build_response_matrix(measured, true)
 
 # np.set_printoptions(precision=3, suppress=True, linewidth=200)
 # print(A)
+plt.matshow(A)
 
 
 ###################################
 ## Fit the basis function coefficients to get the unfolded function f(x)
 tck = blobel_unfold.fit_basis_coefficents(measured)
-
+# print(tck[1])
 
 ####################################
 ## Comparison plots
 plt.figure()
 
-# Plot the columns Aj of A containing the histograms of mc_meas when f0(x)=pj(x). The sum should be the distribution mc_meas.
+## Plot the columns Aj of A containing the histograms of mc_meas when f0(x)=pj(x). The sum should be the distribution mc_meas.
 n_splines = np.shape(A)[1]
 for j in range(n_splines):
-	# Use the left bin border and extend value to the right for plotting. So the last element of bins_meas is to much and has to be taken out. This gives the same plot as an plt.hist() with histtype=step.
+# 	# Use the left bin border and extend value to the right for plotting. So the last element of bins_meas is to much and has to be taken out. This gives the same plot as an plt.hist() with histtype=step.
 	plt.step(bins_meas[:-1], A[:, j], where="post", alpha=0.5, lw=2)
 
 # Plot the sum of the single hists in Aj
 plt.step(bins_meas[:-1], np.sum(A, axis=1), "k", where="post", label="sum(Aj)", lw=2)
-
-# Compare with mc_meas, mc_truth and true pdf. Renorm pdf to fit the hists.
-norm_truth = np.dot(np.diff(bins_meas), np.histogram(true, bins_meas)[0])
-plt.plot(x, norm_truth * pdf, "k-", lw=1, color="red", label="true pdf (renormed)")
-plt.hist(true, bins=bins_meas, normed=False, label="mc_truth", histtype='stepfilled', color="grey", alpha=.5)
-plt.hist(measured, bins=bins_meas, normed=False, label="mc_meas", histtype='stepfilled', color="blue", alpha=.2)
-
-# Plot the unfolded function f(x) from the fitted coefficents aj
-plt.plot(x, norm_truth*sci.splev(x, (tck), ext=1), color="red", lw=2, label="unfolded")
 
 # Plot parameters
 plt.xlim(-0.5, 3.5)
 plt.xlabel("x")
 plt.ylabel("num of entries")
 plt.title("Decomposition of measured MC in basis functions")
+plt.legend(loc="best")
+
+## Compare with mc_meas, mc_truth and true pdf. Renorm pdf to fit the hists.
+plt.figure()
+# norm_truth = np.dot(np.diff(bins_meas), np.histogram(true, bins_meas)[0])
+plt.plot(x, pdf, "k-", lw=1, color="red", label="true pdf (renormed)")
+plt.hist(true, bins=bins_meas, normed=False, label="mc_truth", histtype='stepfilled', color="grey", alpha=.5)
+plt.hist(measured, bins=bins_meas, normed=False, label="mc_meas", histtype='stepfilled', color="blue", alpha=.2)
+
+# Plot the unfolded function f(x) from the fitted coefficents aj
+plt.plot(x, sci.splev(x, (tck), ext=1), color="red", lw=2, label="unfolded")
+
+# Plot parameters
+plt.xlim(-0.5, 3.5)
+plt.xlabel("x")
+plt.ylabel("Probability")
+plt.title("Normed true, measured MC and unfolded pdf f(x)")
 plt.legend(loc="best")
 
 plt.show()

unfold.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 import numpy as np
 import scipy.interpolate as sci
 import scipy.optimize as sco
@@ -43,13 +44,13 @@ class Blobel():
 
 		## Add necessary outer knots by repeating the first and last knot spline_deg times. Used from splines/bsplines.py.
 		# Switch the outer knots creation technique
-		mirror = False
+		mirror = True
 		pre = np.zeros(self.spline_deg)
 		post = np.zeros(self.spline_deg)
 		if mirror:
 			# Mirror internal knots at t0, tn-1
-			pre = ( t[0] - (t[1:nouter+1] - t[0]) )[::-1]
-			post = ( t[-1] + (t[-1] - t[-nouter-1:-1]) )[::-1]
+			pre = ( t[0] - (t[1:self.spline_deg+1] - t[0]) )[::-1]
+			post = ( t[-1] + (t[-1] - t[-self.spline_deg-1:-1]) )[::-1]
 		else:
 			# Just repeat the outermost knots p times each
 			pre.fill(t[0])
@@ -106,16 +107,19 @@ class Blobel():
 			self.spline_coeff[j] = 1
 			# Set the hist weight for Aj(y) proportional to pj(x)
 			tck = (self.spline_knots, self.spline_coeff, self.spline_deg)
-			basis_hist_weights = sci.splev(mc_meas, tck, ext=1)
+			basis_hist_weights = sci.splev(mc_truth, tck, ext=1)
 			# Bin the measured MC with basis_hist_weights according to f0(x)=pj(x)
 			hist, bins = np.histogram(
 				mc_meas,
 				bins=self.bins_meas,
 				range=(self.bins_meas[0], self.bins_meas[1]),
-				normed=False,
+				density=False,
 				weights = basis_hist_weights
 				)
 			self.A[:, j] = hist
+
+		self.A = self.A / self.A.sum(axis=1)[:,np.newaxis]
+		self.A = self.A * np.diff(bins)[:,np.newaxis]
 
 		return self.A
 
@@ -188,11 +192,11 @@ class Blobel():
 			# Calculate the value of g(y) for the current coefficents
 			g_fitted = np.dot(self.A, a)
 
-			## Effectively constrain to positive coefficents by pushing up the neg log-likelihood when aj<0 occurs
+			## Catch log(0) cases
 			# Init with 0. If none of the below is true this is either the case lim x->0 (x*log x) = 0 or 0*log(non-zero) = 0
 			measxlog = np.zeros_like(g_fitted)
 			for i in range(len(g_fitted)):
-				# Case: non-zero value * log(0) = -inf -> return +inf for negllh
+				# Case: non-zero * log(0) = -inf -> return +inf for negllh
 				if (g_meas[i]>0) & (g_fitted[i]<=0):
 					return np.inf
 				# Case: if both values are non-zero/finite then just calculate normally
@@ -206,26 +210,24 @@ class Blobel():
 			mc_meas,
 			bins=self.bins_meas,
 			range=(self.bins_meas[0], self.bins_meas[1]),
-			normed=False
+			density=True
 			)
 
 		# Fit coefficients a, starting from aj = 1 for every j. Coefficients must be positive.
 		# bounds = [[0, None] for i in range(self.n_splines)]
 		x0 = np.ones_like(self.spline_coeff)
-		x0.fill(1.1)
+		# x0.fill(1.1)
 		opt_res = sco.minimize(
 			negllh,
 			x0=x0,
 			args=(g_meas),
+			# jac=False,
 			# bounds=bounds,
 			method="Powell"
 			)
 
 		# Get the basis function coefficients from the result
 		spline_true_coeff = opt_res.x
-
-		print "Succes: {}".format(opt_res.success)
-		print "Num of iter: {}".format(opt_res.nit)
 
 		# Return tck tuple for scipy.interpolate.splev
 		return (self.spline_knots, spline_true_coeff, self.spline_deg)